aa313b46eab2abacaa29df3f8cd563c28adb7757
sbframee <104187401+sbframee@users.noreply.github.com>
2024-08-17 20:53:34 +0530

    first commit

--- src/Pages/topNavbar.jsx
+++ src/Pages/topNavbar.jsx
@@ -15,7 +15,7 @@ export default function TopNavbar() {
     <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
         <a href="https://flowbite.com" className="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SanjuSK</span>
         </a>
         <div className="flex items-center space-x-6 rtl:space-x-reverse">
             <a href="#" className="text-sm  text-blue-600 dark:text-blue-500 hover:underline" onClick={logout}>Logout</a>
