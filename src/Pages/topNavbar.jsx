5eefb736b0d4650ff977361304d5c70533aaa004
mesanjusk <mesanjusk@gmail.comm>
2024-08-19 15:28:46 +0530

    Sanju

--- src/Pages/topNavbar.jsx
+++ src/Pages/topNavbar.jsx
@@ -15,7 +15,7 @@ export default function TopNavbar() {
     <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
         <a href="https://flowbite.com" className="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SanjuSK</span>
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-black">SanjuSK</span>
         </a>
         <div className="flex items-center space-x-6 rtl:space-x-reverse">
             <a href="#" className="text-sm  text-blue-600 dark:text-blue-500 hover:underline" onClick={logout}>Logout</a>
@@ -27,16 +27,16 @@ export default function TopNavbar() {
         <div className="flex items-center">
             <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
                 <li>
-                    <a href="#" className="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</a>
+                    <a href="#" className="text-gray-900 dark:text-white hover:underline" aria-current="page">Orders</a>
                 </li>
                 <li>
-                    <a href="#" className="text-gray-900 dark:text-white hover:underline">Company</a>
+                    <a href="#" className="text-gray-900 dark:text-white hover:underline">Accounts</a>
                 </li>
                 <li>
-                    <a href="#" className="text-gray-900 dark:text-white hover:underline">Team</a>
+                    <a href="#" className="text-gray-900 dark:text-white hover:underline">Reports</a>
                 </li>
                 <li>
-                    <a href="#" className="text-gray-900 dark:text-white hover:underline">Features</a>
+                    <a href="#" className="text-gray-900 dark:text-white hover:underline">Payable</a>
                 </li>
             </ul>
         </div>
